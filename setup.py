072ab825448a093383f269482373c5063c7b64fe
Konstantin Antselovich <kantselovich@dandb.com>
2015-09-01 12:33:01 -0700

    basic project setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from CustomInstall import CustomInstall
+from setuptools import setup, Command
+
+install_requires = [
+  ]
+
+tests_require = [
+  ]
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "License2Deploy",
+    version = "0.0.1",
+    author = "Dun and Bradstreet",
+    author_email = "license2deploy@dandb.com",
+    description = ("Rolling deploys by changing desired amount of instances AWS EC2 Autoscale Group"),
+    license = "GPLv3",
+    keywords = "AWS EC2 AutoScale Group AMI desired capacity",
+    url = "https://github.com/dandb/License2Deploy",
+    packages=['License2Deploy'],
+    install_requires = install_requires,
+    tests_require = tests_require,
+    extras_require={'test': tests_require},
+    long_description=read('README.md') + '\n\n' + read('CHANGES'),
+    test_suite = 'tests',
+    cmdclass={'install': CustomInstall},
+)
