ba368dfa4bdf51b775b5ed8a9e9fdf2e3fdbba82
Prabhakaran Thatchinamoorthy <thatchinamoorthyp@dnb.com>
2015-09-03 17:24:49 -0500

    INFRASYS-5894: filter the appropriate AutoScaling Group

--- License2Deploy/rolling_deploy.py
+++ License2Deploy/rolling_deploy.py
@@ -1,5 +1,6 @@
 import boto.ec2.autoscale as asg
-from _warnings import filters
+import re
+
 
 
 def return_autoscalinggroup_desired_size(auto_scaling_group):
@@ -24,24 +25,28 @@ def update_ami_id_with_last_successfull():
     """ AWS Call: As health check failed, update the ami_id with the last successfull one """
     pass
 
-def retrieve_auto_scaling_group_by_tagname(tag_name, tag_value):
+def retrieve_auto_scaling_group(app_name, environment):
     """ AWS Call: retrieve the Auto Scaling Group by Tag """
-    tag_name = "cloud"
-    tag_value = "dev-cirrus-cache-microservice"
-    asg_connection = asg.connect_to_region("us-east-1")
-    all_tags = asg_connection.get_all_tags()
-    auto_scaling_group_id = ""
-    for tag in all_tags:
-        if ((tag.key == tag_name) and (tag.value == tag_value)) :
-            auto_scaling_group_id = tag.resource_id
-    auto_scalling_group = asg_connection.get_all_groups(names=[auto_scaling_group_id])
-    return auto_scalling_group
-    
+    auto_scaling_groups = asg.connect_to_region("us-east-1").get_all_groups();
+    return filter_auto_scaling_group(auto_scaling_groups, app_name, environment)
+
+def filter_auto_scaling_group(auto_scaling_groups, app_name, project_name, environment):
+    """ Filter out the correct auto scaling group """
+    pattern = app_name + "-backend-" + environment+ "-" + app_name + project_name + "ASG*"
+    for auto_scaling_group in auto_scaling_groups:
+        if (re.match(pattern, auto_scaling_group.name)):
+            return auto_scaling_group
+        
+def wait_for_new_instances_to_spun_up():
+    """  check the status of the instance and wait for it to startup and become available for testing. """
+    pass
+
 def rolling_deploy():
-    auto_scaling_group = retrieve_auto_scaling_group_by_tagname()
+    auto_scaling_group = retrieve_auto_scaling_group()
     current_desired_ec2_count = return_autoscalinggroup_desired_size(auto_scaling_group)
     new_desired_ec2_count = calculate_new_desired_ec2_count(current_desired_ec2_count)
     update_desired_ec2_count_in_autoscalinggroup(auto_scaling_group, new_desired_ec2_count)
+    wait_for_new_instances_to_spun_up()
     if (healthcheck_new_instances()):
         print "Health Check Passed"
         update_desired_ec2_count_in_autoscalinggroup(auto_scaling_group, current_desired_ec2_count)
--- tests/rolling_deploy_test.py
+++ tests/rolling_deploy_test.py
@@ -1,27 +1,26 @@
 
-import unittest
 import License2Deploy.rolling_deploy 
+from boto.ec2.autoscale.group import AutoScalingGroup
+import unittest
+
 import boto.ec2.autoscale as asg
 
 
 class Test(unittest.TestCase):
     
-    def test_return_min_ec2_count(self):
-        desired_count = License2Deploy.rolling_deploy.return_autoscalinggroup_desired_size()
-        self.assertEqual(5, desired_count)
+    def test_filter_auto_scaling_group(self):
+        """ Call: retrieve the Auto Scaling Group by Tag """
+        auto_scaling_groups = []
+        auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbibillingASGstg-1RYWS3F6LT9PW"))
+        auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbicacheASGstg-1O3HO09P2AHYJ"))
+        auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbicirrusASGstg-1GRJQVHPS4COJ"))
+        auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbiemailASGstg-1T3DV362TS505"))
+        auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbigmsextenderASGstg-62N1XQ4MRHSH"))
+        
+        desired_auto_scaling_group = License2Deploy.rolling_deploy.filter_auto_scaling_group(auto_scaling_groups, app_name="dnbi", project_name="billing", environment="stg")
+        
+        self.assertEqual("dnbi-backend-stg-dnbibillingASGstg-1RYWS3F6LT9PW", desired_auto_scaling_group.name)
     
-    def test_retrieve_auto_scaling_group(self):
-            """ AWS Call: retrieve the Auto Scaling Group by Tag """
-    tag_name = "cloud"
-    tag_value = "dev-cirrus-cache-microservice"
-    asg_connection = asg.connect_to_region("us-east-1")
-    all_tags = asg_connection.get_all_tags()
-    auto_scaling_group_id = ""
-    for tag in all_tags:
-        if ((tag.key == tag_name) and (tag.value == tag_value)) :
-            auto_scaling_group_id = tag.resource_id
-    auto_scalling_group = asg_connection.get_all_groups(names=[auto_scaling_group_id])
-    print "->", auto_scalling_group
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testDesiredEC2Count']
