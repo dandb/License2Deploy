8b4d8b10ff8cd3c292df29a6b427533dbbe6084c
Prabhakaran Thatchinamoorthy <thatchinamoorthyp@dnb.com>
2015-09-04 10:02:42 -0500

    INFRASYS-5894: added unit etests with mock

--- License2Deploy/rolling_deploy.py
+++ License2Deploy/rolling_deploy.py
@@ -1,56 +1,52 @@
 import boto.ec2.autoscale as asg
 
+class RollingDeploy:
 
-def init(self):
-    self.aws_connection = asg.connect_to_region('us-east-1')
-
-def return_autoscalinggroup_desired_size(auto_scaling_group):
-    """ AWS Call: return the  minsize of the AutoScaling Group of the Microservice"""
-    return auto_scaling_group.min_size
-
-def calculate_new_desired_ec2_count(current_desired_ec2_count):
-    """ By default, we double the instance Count """
-    return 2 * current_desired_ec2_count
-
-def update_desired_ec2_count_in_autoscalinggroup(auto_scaling_group, ec2_count):
-    auto_scaling_group.set_capacity(ec2_count)
-    """ AWS Call: update the desired_size of the AutoScaling Group of the Microserice"""
-    pass
-
-def healthcheck_new_instances():
-    """ AWS Call: Check whether the new instances are working good """
-    return True
-
-def retrieve_auto_scaling_group(self, app_name, project_name, environment):
-    """ AWS Call: retrieve the Auto Scaling Group by Tag """
-    auto_scaling_groups = self.aws_connection.get_all_groups();
-    return filter_auto_scaling_group(auto_scaling_groups, app_name, project_name, environment)
-
-def filter_auto_scaling_group(auto_scaling_groups, app_name, project_name, environment):
-    """ Filter out the correct auto scaling group """
-    pattern = app_name + "-backend-" + environment+ "-" + app_name + project_name
-    for auto_scaling_group in auto_scaling_groups:
-        if (auto_scaling_group.name.find(pattern) > -1):
-            return auto_scaling_group
-        
-def wait_for_new_instances_to_spun_up():
-    """  check the status of the instance and wait for it to startup and become available for testing. """
-    pass
-
-def rolling_deploy():
-    auto_scaling_group = retrieve_auto_scaling_group()
-    current_desired_ec2_count = return_autoscalinggroup_desired_size(auto_scaling_group)
-    new_desired_ec2_count = calculate_new_desired_ec2_count(current_desired_ec2_count)
-    update_desired_ec2_count_in_autoscalinggroup(auto_scaling_group, new_desired_ec2_count)
-    wait_for_new_instances_to_spun_up()
-    if (healthcheck_new_instances()):
-        print "Health Check Passed"
-        update_desired_ec2_count_in_autoscalinggroup(auto_scaling_group, current_desired_ec2_count)
-    else:
-        print "Health Check Failed"
-        return "Error"
-        
-if __name__ == '__main__':
-    rolling_deploy()  
+    def init(self):
+        self.aws_connection = asg.connect_to_region('us-east-1')
     
-   
+    def return_autoscalinggroup_desired_size(self, auto_scaling_group):
+        """ AWS Call: return the  minsize of the AutoScaling Group of the Microservice"""
+        return auto_scaling_group.min_size
+    
+    def calculate_new_desired_ec2_count(self, current_desired_ec2_count):
+        """ By default, we double the instance Count """
+        return 2 * current_desired_ec2_count
+    
+    def update_desired_ec2_count_in_autoscalinggroup(self, auto_scaling_group, ec2_count):
+        auto_scaling_group.set_capacity(ec2_count)
+        """ AWS Call: update the desired_size of the AutoScaling Group of the Microserice"""
+        pass
+    
+    def healthcheck_new_instances(self):
+        """ AWS Call: Check whether the new instances are working good """
+        return True
+    
+    def retrieve_auto_scaling_group(self, app_name, project_name, environment):
+        """ AWS Call: retrieve the Auto Scaling Group by Tag """
+        auto_scaling_groups = self.aws_connection.get_all_groups();
+        return self.filter_auto_scaling_group(self, auto_scaling_groups, app_name, project_name, environment)
+    
+    def filter_auto_scaling_group(self, auto_scaling_groups, app_name, project_name, environment):
+        """ Filter out the correct auto scaling group """
+        pattern = app_name + "-backend-" + environment+ "-" + app_name + project_name
+        for auto_scaling_group in auto_scaling_groups:
+            if (auto_scaling_group.name.find(pattern) > -1):
+                return auto_scaling_group
+            
+    def wait_for_new_instances_to_spun_up(self):
+        """  check the status of the instance and wait for it to startup and become available for testing. """
+        pass
+    
+    def rolling_deploy(self):
+        auto_scaling_group = self.retrieve_auto_scaling_group(self)
+        current_desired_ec2_count = self.return_autoscalinggroup_desired_size(self, auto_scaling_group)
+        new_desired_ec2_count = self.calculate_new_desired_ec2_count(self, current_desired_ec2_count)
+        self.update_desired_ec2_count_in_autoscalinggroup(self, auto_scaling_group, new_desired_ec2_count)
+        self.wait_for_new_instances_to_spun_up(self)
+        if (self.healthcheck_new_instances(self)):
+            print "Health Check Passed"
+            self.update_desired_ec2_count_in_autoscalinggroup(self, auto_scaling_group, current_desired_ec2_count)
+        else:
+            print "Health Check Failed"
+            return "Error"
--- tests/rolling_deploy_test.py
+++ tests/rolling_deploy_test.py
@@ -1,14 +1,18 @@
 
 import License2Deploy.rolling_deploy 
 from boto.ec2.autoscale.group import AutoScalingGroup
+from boto.ec2.autoscale import AutoScaleConnection
+from License2Deploy.rolling_deploy import RollingDeploy
 import unittest
+from mock import MagicMock
 
 import boto.ec2.autoscale as asg
+
 
 class Test(unittest.TestCase):
     
     def test_filter_auto_scaling_group(self):
-        """ Call: retrieve the Auto Scaling Group by Tag """
+        mock_autoscale_connection = AutoScaleConnection()
         auto_scaling_groups = []
         auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbibillingASGstg-1RYWS3F6LT9PW"))
         auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbicacheASGstg-1O3HO09P2AHYJ"))
@@ -16,9 +20,22 @@ class Test(unittest.TestCase):
         auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbiemailASGstg-1T3DV362TS505"))
         auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbigmsextenderASGstg-62N1XQ4MRHSH"))
         
-        desired_auto_scaling_group = License2Deploy.rolling_deploy.filter_auto_scaling_group(auto_scaling_groups, app_name="dnbi", project_name="billing", environment="stg")
+        mock_autoscale_connection.get_all_groups = MagicMock(return_value=auto_scaling_groups) 
+        asg.connect_to_region = MagicMock(return_value=mock_autoscale_connection)
         
-        self.assertEqual("dnbi-backend-stg-dnbibillingASGstg-1RYWS3F6LT9PW", desired_auto_scaling_group.name)
+        rolling_deploy = RollingDeploy()
+        desired_auto_scaling_group = rolling_deploy.filter_auto_scaling_group(auto_scaling_groups, app_name="dnbi", project_name="email", environment="stg")
+        
+        self.assertEqual("dnbi-backend-stg-dnbiemailASGstg-1T3DV362TS505", desired_auto_scaling_group.name)
+    
+        
+         
+        
+        
+
+        
+        
+    
     
 
 if __name__ == "__main__":
