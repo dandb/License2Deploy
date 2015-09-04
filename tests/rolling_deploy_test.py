330dfe653da7fb801da8d872c7bc39387709762b
Prabhakaran Thatchinamoorthy <thatchinamoorthyp@dnb.com>
2015-09-04 11:21:49 -0500

    INFRASYS-5894: updated the tests

--- tests/rolling_deploy_test.py
+++ tests/rolling_deploy_test.py
@@ -1,11 +1,11 @@
 
-import License2Deploy.rolling_deploy 
-from boto.ec2.autoscale.group import AutoScalingGroup
-from boto.ec2.autoscale import AutoScaleConnection
 from License2Deploy.rolling_deploy import RollingDeploy
-import unittest
+import License2Deploy.rolling_deploy 
+from boto.ec2.autoscale import AutoScaleConnection
+from boto.ec2.autoscale.group import AutoScalingGroup
 from mock import MagicMock
-
+from mock.mock import patch
+import unittest
 
 import boto.ec2.autoscale as asg
 
@@ -13,7 +13,6 @@ import boto.ec2.autoscale as asg
 class Test(unittest.TestCase):
     
     def test_filter_auto_scaling_group(self):
-        
         mock_autoscale_connection = MagicMock(spec=AutoScaleConnection)
         auto_scaling_groups = []
         auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbibillingASGstg-1RYWS3F6LT9PW"))
@@ -23,14 +22,14 @@ class Test(unittest.TestCase):
         auto_scaling_groups.append(AutoScalingGroup(name="dnbi-backend-stg-dnbigmsextenderASGstg-62N1XQ4MRHSH"))
         mock_autoscale_connection.get_all_groups.return_value = auto_scaling_groups 
         
-        print "All Groups=", mock_autoscale_connection.get_all_groups
+        print  mock_autoscale_connection.get_all_groups
         
         mock_rolling_deploy = MagicMock(spec=RollingDeploy)
         mock_rolling_deploy.aws_connection = mock_autoscale_connection
         
         desired_auto_scaling_group = mock_rolling_deploy.retrieve_auto_scaling_group(app_name="dnbi", project_name="email", environment="stg")
-        print "Group=", desired_auto_scaling_group
-        
+        #print "Group=", desired_auto_scaling_group
+        print vars(desired_auto_scaling_group)
         self.assertEqual("dnbi-backend-stg-dnbiemailASGstg-1T3DV362TS505", desired_auto_scaling_group)
     
 
